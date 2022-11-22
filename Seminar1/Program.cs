ba790a8a995a0f5c38099f23844130ed654ac221
Iryna <batsianiryna@gmail.com>
2022-11-22 12:13:48 +0300

    add Seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 2 == 0 || n % 3 == 0)
+  Console.Write("YES");
+else
+  Console.Write("NO");
