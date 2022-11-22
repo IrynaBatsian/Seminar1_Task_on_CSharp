0d0de6ce28fb58cd81c82f33e83b93373f9aaa44
Iryna <batsianiryna@gmail.com>
2022-11-22 12:49:48 +0300

    add info in Seminar1 Task2

--- Seminar1_Task2/Program.cs
+++ Seminar1_Task2/Program.cs
@@ -3,7 +3,7 @@ Console.Write("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a * a == b)
+if (b * b == a)
   Console.Write("YES");
 else
   Console.Write("NO");
