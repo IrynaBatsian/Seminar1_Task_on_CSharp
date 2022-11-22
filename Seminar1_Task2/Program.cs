2ef1a47d787165adce92dc30e27548b82c8feab5
Iryna <batsianiryna@gmail.com>
2022-11-22 12:47:46 +0300

    add Seminar1 Task2

---
+++ Seminar1_Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b)
+  Console.Write("YES");
+else
+  Console.Write("NO");
+
