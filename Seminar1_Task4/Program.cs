8f55adce02c43268fc11f2ff81c8987f367f88f2
Iryna <batsianiryna@gmail.com>
2022-11-22 14:22:34 +0300

    add Seminar1 Task4

---
+++ Seminar1_Task4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = (-1) * n; i <= n; i++)
+  Console.Write($"{i} "); //интерполяция(соединение строк)
+  
+
